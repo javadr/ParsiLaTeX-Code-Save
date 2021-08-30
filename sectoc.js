5121b5793012a7e93f70ebf26d129e751f1fbd46
javad <javadr@gmail.com>
2021-08-30 16:21:24 +0430

    Highlighting the code box

--- sectoc.js
+++ sectoc.js
@@ -1,14 +1,19 @@
 // Courtesy to main author Martin Schmelzer schmelzer.martin@gmx.de
 // Modified by Javad Razavian javadr@gmail.com
 
-$chks = $('div.qa-part-q-view, div.qa-part-a-list');
+$chks = $('div.qa-part-q-view, div.qa-part-a-list, div#question, div#answers, div.qist-data');
 
-// The following lines are there to debug what elements got selected
-//$chks.on('mouseover', 'pre, span > code, p > code', function() {
-// 	$(this).css('background-color', '#FF0000');
-//});
+// The following lines highlight what elements got selected
+$chks.on('mouseover', 'pre, span > code, p > code, li > code', function() {
+  $border = $(this).css("border");
+ 	$(this).css('border', 'solid');
+});
+$chks.on('mouseout', 'pre, span > code, p > code, li > code', function() {
+ 	$(this).css('border', $border);
+});
 
-$chks.on('dblclick', 'pre, span > code, p > code', function() {
+// Main part which copies the text to the clipboard
+$chks.on('dblclick', 'pre, span > code, p > code, li > code', function() {
 	var $flash = $(this);
 	var $code = ($(this).is('pre')) ? ($(this).children('code')) : ($(this));
 	
