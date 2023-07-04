c87d3a18c9610983c3da788fb82a403049c9c917
javad <javadr@gmail.com>
2023-07-04 12:20:12 +0330

    (style) clean code

--- sectoc.js
+++ sectoc.js
@@ -5,25 +5,25 @@ $chks = $('div.qa-part-q-view, div.qa-part-a-list, div#question, div#answers, di
 
 // The following lines highlight what elements got selected
 $chks.on('mouseover', 'pre, span > code, p > code, li > code', function() {
-  $border = $(this).css("border");
- 	$(this).css('border', 'solid');
+	$border = $(this).css("border");
+		$(this).css('border', 'solid');
 });
 $chks.on('mouseout', 'pre, span > code, p > code, li > code', function() {
- 	$(this).css('border', $border);
+	$(this).css('border', $border);
 });
 
 // Main part which copies the text to the clipboard
 $chks.on('dblclick', 'pre, span > code, p > code, li > code', function() {
 	var $flash = $(this);
 	var $code = ($(this).is('pre')) ? ($(this).children('code')) : ($(this));
-	
+
 	var $temp = $("<textarea>").appendTo("body");
 	$temp.val($code.text()).select();
 	if(document.execCommand("copy")) {
 		$flash.addClass("soctoc");
 		setTimeout(function () {
- 	    	$flash.removeClass("soctoc");
-		}, 410);	
+			$flash.removeClass("soctoc");
+		}, 410);
 	};
 	$temp.remove();
 });
