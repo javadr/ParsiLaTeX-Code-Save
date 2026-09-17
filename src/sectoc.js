03b68962f441f875130403139e244a1acc34d940
Javad Razavian <javadr@gmail.com>
2026-09-17 22:04:21 +0200

    Fix hover highlighting and bump version to 0.2.1

--- src/sectoc.js
+++ src/sectoc.js
@@ -1,15 +1,14 @@
 // Courtesy to main author Martin Schmelzer schmelzer.martin@gmx.de
 // Modified by Javad Razavian javadr@gmail.com
 
-$chks = $('div.qa-part-q-view, div.qa-part-a-list, div#question, div#answers, div.qist-data');
+const $chks = $('div.qa-part-q-view, div.qa-part-a-list, div#question, div#answers, div.qist-data');
 
 // The following lines highlight what elements got selected
-$chks.on('mouseover', 'pre, span > code, p > code, li > code', function() {
-	$border = $(this).css("border");
-		$(this).css('border', 'solid');
+$chks.on('mouseenter', 'pre, span > code, p > code, li > code', function() {
+	$(this).addClass('parsilatex-code-save-hover');
 });
-$chks.on('mouseout', 'pre, span > code, p > code, li > code', function() {
-	$(this).css('border', $border);
+$chks.on('mouseleave', 'pre, span > code, p > code, li > code', function() {
+	$(this).removeClass('parsilatex-code-save-hover');
 });
 
 // Main part which copies the text to the clipboard
